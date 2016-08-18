485e3ce0cba5a82012e0e1b46901b0c58f69154e
GondarOleg <_oleg@ua.fm>
2016-08-18 08:56:16 +0300

    Initial commit

---
+++ src/main/java/Run.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Hondar.O.M-ad on 18.08.2016.
+ */
+public class Run {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static long sub(long a, long b){
+        return a - b;
+    }
+
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,12 @@
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Tests {
+
+    @Test
+    public void checkSub(){
+        assertTrue(2 == Run.sub(4,2));
+    }
+
+}
